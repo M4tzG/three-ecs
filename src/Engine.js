507ffaf72368509fc64dea0b6535a06ffaa28b93
Matheus Gabriel <99271709+M4tzG@users.noreply.github.com>
2026-05-20 17:09:21 -0300

    teste

--- src/Engine.js
+++ src/Engine.js
@@ -1,8 +1,45 @@
+import * as THREE from "three"
+
 export default class Engine {
     constructor(){
         this.test_text = "lalalalala";
     }
     test(){
         console.log(this.test_text)
+    }
+
+
+    criarRetangulo(container = document.body) {
+        const scene = new THREE.Scene();
+
+        const camera = new THREE.OrthographicCamera(
+            -1,
+            1,
+            1,
+            -1,
+            0.1,
+            10
+        );
+
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        container.appendChild(renderer.domElement);
+
+        const geometry = new THREE.PlaneGeometry(1, 1);
+
+        const material = new THREE.MeshBasicMaterial({
+            color: 0x00ff00
+        });
+
+        const rect = new THREE.Mesh(geometry, material);
+
+        scene.add(rect);
+
+        camera.position.z = 1;
+
+        renderer.render(scene, camera);
+
+        console.log("retangulo criado");
     }
 }
